b7694ad8e0ac1e2f3ba307b23051716b91657f22
DIZI <qweadszxc88@gmail.com>
2025-09-04 05:20:38 +0300

    products are split into lists

--- imagApp/models.py
+++ imagApp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-class Product(models.Model):
-    serial_number = models.CharField("Серійний номер", max_length=50, unique=True)
+class ProductGroup(models.Model):
+    serial_number = models.CharField("Серійний номер групи", max_length=50, unique=True)
+    name = models.CharField("Назва групи", max_length=100)
+    price = models.DecimalField("Ціна (євро)", max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"{self.serial_number} - {self.name}"
+
+class Product(models.Model):
+    group = models.ForeignKey(ProductGroup, on_delete=models.CASCADE, related_name="products")
+    serial_number = models.CharField("Серійний номер", max_length=50)
     name  = models.CharField("Виріб", max_length=100)
     price = models.DecimalField("Ціна (євро)", max_digits=10, decimal_places=2)
     stock = models.IntegerField("Кількість")
--- imagApp/urls.py
+++ imagApp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("production/<int:pk>/edit/", views.product_edit, name="product_edit"),
     path("production/<int:pk>/delete/", views.product_delete, name="product_delete"),
     path("print/", views.print_3d, name="print_3d"),
-
+    path("group/add/", views.add_group, name="add_group"),
+    path("group/<int:pk>/edit/", views.edit_group, name="edit_group")
 ]
